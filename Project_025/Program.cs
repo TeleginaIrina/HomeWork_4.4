252a77e28c68f71a1e38440de581bd04fb25cf0f
Телегина Ирина <ikoska056@gmail.com>
2022-04-30 16:50:07 +0300

    project_025

---
+++ Project_025/Program.cs
@@ -0,0 +1,17 @@
+﻿// 26. Возведите число А в натуральную степень B используя цикл
+Console.Clear();
+System.Console.WriteLine();
+
+int number = new Random().Next(1, 100);
+int exponent = new Random().Next(1, 10);
+int count = 1;
+int result = number;
+
+while (count < exponent)
+{
+    result = result * number;
+    count++;
+}
+
+System.Console.WriteLine($"{number} ^ {exponent} = {result}");
+System.Console.WriteLine();
